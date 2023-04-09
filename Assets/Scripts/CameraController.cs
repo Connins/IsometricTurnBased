6ecb1d29006e9d8b2ef7195d140dc515e76a1a3b
Connins <connins@hotmail.co.uk>
2023-04-09 22:44:35 +0200

    Added in camera rudimentary rotation
    This rotation is not slerping so instant snapping also the position of camera is not on same square after rotating but will be on same relative square to the new corner

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
 
 public class CameraController : MonoBehaviour
 {
@@ -9,12 +10,16 @@ public class CameraController : MonoBehaviour
 
     [SerializeField] private float moveSpeed = 10f;
 
+    [SerializeField] private float cameraMoveSize = 35;
+    private int angle = 0;
+    private List<Vector3> targetPositon = new List<Vector3>();
+
     float horInput;
     float verInput;
     // Start is called before the first frame update
     void Start()
     {
-
+        SetTargetPositoins();
     }
 
     // Update is called once per frame
@@ -22,6 +27,15 @@ public class CameraController : MonoBehaviour
     {
         HandleInput();
         Move();
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Rotate(false);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Rotate(true);
+        }
     }
     private void HandleInput()
     {
@@ -35,5 +49,49 @@ public class CameraController : MonoBehaviour
         Vec.x += horInput * Time.deltaTime * moveSpeed;
         Vec.y += verInput * Time.deltaTime * moveSpeed;
         transform.localPosition = Vec;
+    }
+
+    private void Rotate(bool clockWise)
+    {
+        Quaternion targetRotation;
+        int Rotate;
+        float smooth = 5f;
+        if (clockWise) 
+        {
+            angle = angle + 1;
+            if (angle == 4)
+            {
+                angle = 0;
+            }
+            transform.parent.Translate(targetPositon[angle], Space.World);
+
+            targetRotation = Quaternion.Euler(0,  90, 0);
+            Rotate = 90;
+        }
+        else
+        {
+            transform.parent.Translate(-targetPositon[angle], Space.World);
+
+            angle = angle - 1;
+            if (angle == -1)
+            {
+                angle = 3;
+            }
+            targetRotation = Quaternion.Euler(0, -90, 0);
+            Rotate = -90;
+        }
+        
+        
+        //transform.parent.rotation = Quaternion.Slerp(transform.localToWorldMatrix.rotation, targetRotation, Time.deltaTime * smooth);
+        //transform.parent.position = Vector3.Slerp(transform.parent.position, targetPositon[angle], Time.deltaTime * smooth);
+        transform.parent.Rotate(0,Rotate,0, Space.World);
+    }
+
+    private void SetTargetPositoins()
+    {
+        targetPositon.Add(new Vector3(-cameraMoveSize, 0, 0));
+        targetPositon.Add(new Vector3(0, 0, cameraMoveSize));
+        targetPositon.Add(new Vector3(cameraMoveSize, 0, 0));
+        targetPositon.Add(new Vector3(0, 0, -cameraMoveSize));
     }
 }
